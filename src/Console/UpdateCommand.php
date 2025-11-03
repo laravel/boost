c1e678c949419ef70dd6357ca02cb802b8f2c434
Pushpak Chhajed <pushpak1300@gmail.com>
2025-11-03 20:11:15 +0530

    ignore mcp config update in boost:update
    Signed-off-by: Pushpak Chhajed <pushpak1300@gmail.com>

--- src/Console/UpdateCommand.php
+++ src/Console/UpdateCommand.php
@@ -14,6 +14,7 @@ class UpdateCommand extends Command
     {
         $this->callSilently(InstallCommand::class, [
             '--no-interaction' => true,
+            '--ignore-mcp-config' => true,
         ]);
 
         $this->components->info('Boost guidelines updated successfully.');
