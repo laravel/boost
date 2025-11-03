251157e2b3de07475a0cb385d03636fa1ff35409
Pushpak Chhajed <pushpak1300@gmail.com>
2025-11-03 20:53:21 +0530

    Add type annotation for `$signature` in `InstallCommand`
    Signed-off-by: Pushpak Chhajed <pushpak1300@gmail.com>

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -34,6 +34,7 @@ class InstallCommand extends Command
 {
     use Colors;
 
+    /** @var string */
     protected $signature = 'boost:install {--ignore-guidelines : Skip installing AI guidelines} {--ignore-mcp : Skip installing MCP server configuration}';
 
     private CodeEnvironmentsDetector $codeEnvironmentsDetector;
