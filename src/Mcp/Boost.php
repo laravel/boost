cd60595531dd568a73c792453a82c487ff4ff069
Pushpak Chhajed <pushpak@laravel.com>
2026-01-11 17:32:18 +0530

    Add Laravel Code Simplifier Prompt (#416)
    * Add Laravel Code Simplifier resource
    
    Signed-off-by: Pushpak Chhajed <pushpak1300@gmail.com>
    
    * Formatting
    
    Signed-off-by: Pushpak Chhajed <pushpak1300@gmail.com>
    
    * Formatting
    
    Signed-off-by: Pushpak Chhajed <pushpak1300@gmail.com>
    
    * Formatting
    
    Signed-off-by: Pushpak Chhajed <pushpak1300@gmail.com>
    
    * Change from Resource to Prompts
    
    Signed-off-by: Pushpak Chhajed <pushpak1300@gmail.com>
    
    ---------
    
    Signed-off-by: Pushpak Chhajed <pushpak1300@gmail.com>

--- src/Mcp/Boost.php
+++ src/Mcp/Boost.php
@@ -6,6 +6,7 @@ namespace Laravel\Boost\Mcp;
 
 use InvalidArgumentException;
 use Laravel\Boost\Mcp\Methods\CallToolWithExecutor;
+use Laravel\Boost\Mcp\Prompts\LaravelCodeSimplifier\LaravelCodeSimplifier;
 use Laravel\Boost\Mcp\Prompts\PackageGuidelinePrompt;
 use Laravel\Boost\Mcp\Resources\PackageGuidelineResource;
 use Laravel\Boost\Mcp\Tools\ApplicationInfo;
@@ -124,10 +125,12 @@ class Boost extends Server
      */
     protected function discoverPrompts(): array
     {
-        return $this->filterPrimitives(
-            $this->discoverThirdPartyPrimitives(Prompt::class),
-            'prompts'
-        );
+        $availablePrompts = [
+            LaravelCodeSimplifier::class,
+            ...$this->discoverThirdPartyPrimitives(Prompt::class),
+        ];
+
+        return $this->filterPrimitives($availablePrompts, 'prompts');
     }
 
     /**
