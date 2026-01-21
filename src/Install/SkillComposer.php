7a7d1761104e89f214033e70fb5fa4de80d3f06e
Pushpak Chhajed <pushpak1300@gmail.com>
2026-01-21 23:00:43 +0530

    Formatting
    Signed-off-by: Pushpak Chhajed <pushpak1300@gmail.com>

--- src/Install/SkillComposer.php
+++ src/Install/SkillComposer.php
@@ -20,10 +20,10 @@ class SkillComposer
     /** @var Collection<string, Skill>|null */
     protected ?Collection $skills = null;
 
-    public function __construct(
-        protected Roster $roster,
-        protected GuidelineConfig $config = new GuidelineConfig
-    ) {}
+    public function __construct(protected Roster $roster, protected GuidelineConfig $config = new GuidelineConfig)
+    {
+        //
+    }
 
     protected function getRoster(): Roster
     {
