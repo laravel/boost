65479d171c1cad84192d2d017c95df87c3a4f8e3
Sam Anglin <sam@mizouzie.dev>
2026-05-19 13:05:04 +0300

    Add tinker tool option to config - Default to false, maintaining opt-in - Give brief explainer of benefits

--- config/boost.php
+++ config/boost.php
@@ -32,6 +32,19 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Boost Tinker Tool (MCP)
+    |--------------------------------------------------------------------------
+    |
+    | The following option may be used to enable or disable the tinker tool MCP
+    | feature within Laravel Boost. The tinker tool can helps when there is
+    | need to execute PHP to debug code or query Eloquent models directly.
+    |
+    */
+
+    'tinker_tool_enabled' => env('BOOST_TINKER_TOOL_ENABLED', false),
+
+    /*
+    |--------------------------------------------------------------------------
     | Boost Executables Paths
     |--------------------------------------------------------------------------
     |
